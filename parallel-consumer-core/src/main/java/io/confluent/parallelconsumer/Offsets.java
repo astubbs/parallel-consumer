3bb0f62d526958d14e25ac0953afdaf596755d92
Antony Stubbs <antony.stubbs@gmail.com>
2022-10-11 11:58:26 +0100

    START: Introduce Offsets and Offset class

---
+++ parallel-consumer-core/src/main/java/io/confluent/parallelconsumer/Offsets.java
@@ -0,0 +1,50 @@
+package io.confluent.parallelconsumer;
+
+/*-
+ * Copyright (C) 2020-2022 Confluent, Inc.
+ */
+
+import lombok.ToString;
+import lombok.Value;
+import lombok.experimental.Delegate;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * A range of {@link Offset}s.
+ */
+@ToString
+public class Offsets {
+
+    @Delegate
+    private final List<Long> rawOffsets;
+
+    public Offsets(List<Long> records) {
+        this.rawOffsets = records;
+    }
+
+    public static Offsets fromRecords(List<RecordContext<?, ?>> records) {
+        return fromLongs(records.stream()
+                .map(RecordContext::offset)
+                .collect(Collectors.toUnmodifiableList()));
+    }
+
+    // due to type erasure, can't use method overloading
+    public static Offsets fromLongs(List<Long> rawOffsetsIn) {
+        return new Offsets(rawOffsetsIn);
+    }
+
+    public static Offsets fromArray(long... rawOffsetsIn) {
+        return new Offsets(Arrays.stream(rawOffsetsIn).boxed().collect(Collectors.toList()));
+    }
+
+    /**
+     * Class value for a Kafka offset, to avoid being Longly typed.
+     */
+    @Value
+    public static class Offset {
+        long value;
+    }
+}
